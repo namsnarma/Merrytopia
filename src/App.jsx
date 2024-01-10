f9c4a22a9e0d9ded537bbbe679bbf3cc921a34d2
L S NARMADHA <narmadha.ls.27@gmail.com>
2024-01-10 09:34:22 +0530

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-
+import { React } from 'react'
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
